cad4c069deca1fbbfb6b8f4d21b6c341cc9c6b55
jtimberman <joshua@opscode.com>
2012-11-13 23:08:15 -0700

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name              "subversion"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs subversion"
-version           "1.0.2"
+version           "1.0.3"
 
 %w{ redhat centos fedora ubuntu debian windows suse }.each do |os|
   supports os
