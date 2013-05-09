0ea2e7d647e3985e26899fb8156ff5dd49a8dde7
Steven Danna <steve@opscode.com>
2013-05-08 20:41:50 -0700

    [COOK-2914] Fix foodcritic warnings.
    Notification style and whitespace.

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,9 +8,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ end
 web_app "subversion" do
   template "subversion.conf.erb"
   server_name "#{node['subversion']['server_name']}.#{node['domain']}"
-  notifies :restart, resources(:service => "apache2")
+  notifies 'restart[apache2]'
 end
 
 execute "svnadmin create repo" do
