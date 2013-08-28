035106c374a0848fb6d6dbf36900dd02d71c2e1c
Seth Vargo <sethvargo@gmail.com>
2013-08-28 18:44:49 -0400

    Bump devodd release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs subversion"
-version           "1.0.4"
+version           "1.0.5"
 
 %w{ redhat centos fedora ubuntu debian windows suse }.each do |os|
   supports os
