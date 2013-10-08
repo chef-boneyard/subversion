2706704c6f20c66e811f6f6ae25e8e25e29d6bb1
Seth Vargo <sethvargo@gmail.com>
2013-10-08 15:29:14 -0400

    Version bump to v1.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs subversion'
-version           '1.1.0'
+version           '1.1.1'
 
 supports 'centos'
 supports 'debian'
