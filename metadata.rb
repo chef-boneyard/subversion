2c4be18c575f36d918974ba0f23bdf2e0ec02056
Tim Smith <tsmith84@gmail.com>
2015-09-08 14:08:29 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -20,6 +20,9 @@ recipe 'subversion', 'Includes the client recipe.'
 recipe 'subversion::client', 'Subversion Client installs subversion and some extra svn libs'
 recipe 'subversion::server', 'Subversion Server (Apache2 mod_dav_svn)'
 
+source_url 'https://github.com/chef-cookbooks/subversion' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/subversion/issues' if respond_to?(:issues_url)
+
 attribute 'subversion/list_parent_path',
           :display_name => 'Subversion List Parent Path?',
           :description  => %q(a choice of "on" or "off".  When set to "on" the list of repositories in the `node['subversion']['repo_dir']` will be indexed at http://<server_name>/svn.  Default is "off"),
