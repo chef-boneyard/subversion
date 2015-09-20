f72b72ba573ce547581414662a778de313d9218f
Tim Smith <tsmith84@gmail.com>
2015-09-19 23:36:17 -0700

    Fix Apache 2 dep to allow for latest version
    This is pulled in from
    https://github.com/chef-cookbooks/subversion/pull/18
    
    Also removed the single attribute and expanded the supported platforms

--- metadata.rb
+++ metadata.rb
@@ -3,17 +3,14 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs subversion'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.0'
 
-supports 'centos'
-supports 'debian'
-supports 'fedora'
-supports 'redhat'
-supports 'suse'
-supports 'ubuntu'
-supports 'windows'
+%w(fedora ubuntu debian redhat centos suse scientific oracle amazon windows).each do |os|
+  supports os
+end
 
-depends 'apache2', '~> 2.0.0'
+depends 'apache2', '>= 2.0.0'
 depends 'windows', '~> 1.10'
 
 recipe 'subversion', 'Includes the client recipe.'
@@ -22,10 +19,3 @@ recipe 'subversion::server', 'Subversion Server (Apache2 mod_dav_svn)'
 
 source_url 'https://github.com/chef-cookbooks/subversion' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/subversion/issues' if respond_to?(:issues_url)
-
-attribute 'subversion/list_parent_path',
-          display_name: 'Subversion List Parent Path?',
-          description: %q(a choice of "on" or "off".  When set to "on" the list of repositories in the `node['subversion']['repo_dir']` will be indexed at http://<server_name>/svn.  Default is "off"),
-          required: 'optional',
-          default: 'off',
-          recipes: ['subversion::server']
