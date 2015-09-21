af36c89816af5301604659ebf1c4f0f2ef231966
Tim Smith <tsmith@chef.io>
2015-09-20 19:14:26 -0700

    Update copyright date

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,7 @@
 # Recipe:: server
 #
 # Copyright 2009, Daniel DeLeo
-# Copyright 2013, Chef
+# Copyright 2013-2015, Chef Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
