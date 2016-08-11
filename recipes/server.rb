cb5ffe2bed1975decf6a1346224b22f941fe271a
Tim Smith <tsmith@chef.io>
2016-08-11 14:40:55 -0700

    Remove the double gate on the htpasswd file
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/server.rb
+++ recipes/server.rb
@@ -48,7 +48,6 @@ package 'apache2-utils' if platform_family?('debian', 'suse') && node['apache'][
 execute 'create htpasswd file' do
   command "htpasswd -scb #{node['subversion']['repo_dir']}/htpasswd #{node['subversion']['user']} #{node['subversion']['password']}"
   creates "#{node['subversion']['repo_dir']}/htpasswd"
-  not_if File.exist?("#{node['subversion']['repo_dir']}/htpasswd")
 end
 
 execute 'update htpasswd file' do
