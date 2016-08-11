7a2e6f4a361e15bafc1c026e93ba403ad1a5d0a9
Jennifer Davis <iennae@gmail.com>
2016-08-11 14:19:19 -0700

    resolving conflict

--- recipes/server.rb
+++ recipes/server.rb
@@ -47,5 +47,10 @@ package 'apache2-utils' if platform_family?('debian', 'suse') && node['apache'][
 
 execute 'create htpasswd file' do
   command "htpasswd -scb #{node['subversion']['repo_dir']}/htpasswd #{node['subversion']['user']} #{node['subversion']['password']}"
-  creates "#{node['subversion']['repo_dir']}/htpasswd"
+  creates "#{node['subversion']['repo_name']}/htpasswd"
+  not_if File.exist?("#{node['subversion']['repo_name']}/htpasswd")
+end
+
+execute 'update htpasswd file' do
+  command "htpasswd -sb #{node['subversion']['repo_dir']}/htpasswd #{node['subversion']['user']} #{node['subversion']['password']}"
 end
