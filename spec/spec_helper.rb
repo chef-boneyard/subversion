9d2c3796ce2b090f95fa2b37b63018b8f4b8f257
Tim Smith <tsmith@chef.io>
2016-09-01 12:24:44 -0700

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-Berkshelf.ui.mute do
-  Berkshelf::Berksfile.from_file('Berksfile').install
+RSpec.configure do |config|
+  config.color = true               # Use color in STDOUT
+  config.formatter = :documentation # Use the specified formatter
+  config.log_level = :error         # Avoid deprecation notice SPAM
 end
