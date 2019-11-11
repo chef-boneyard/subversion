90e2106612d5c9b63fe3c86e1c87254b23319f08
Tim Smith <tsmith84@gmail.com>
2019-11-10 17:50:50 -0800

    Remove long_description and recipe metadata
    No need for these anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs subversion'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
 
 %w(fedora ubuntu debian redhat centos suse scientific oracle amazon windows).each do |os|
@@ -11,10 +10,6 @@ version '3.0.0'
 end
 
 depends 'apache2', '>= 2.0.0'
-
-recipe 'subversion::default', 'Includes the client recipe.'
-recipe 'subversion::client', 'Subversion Client installs subversion and some extra svn libs'
-recipe 'subversion::server', 'Subversion Server (Apache2 mod_dav_svn)'
 
 source_url 'https://github.com/chef-cookbooks/subversion'
 issues_url 'https://github.com/chef-cookbooks/subversion/issues'
